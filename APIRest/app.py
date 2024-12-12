89866d7abe6b54b7d428d4e2af03933c7f30b308
Marina Pintado <marinaguanghua@gmail.com>
2024-12-12 12:21:01 +0100

    app

--- APIRest/app.py
+++ APIRest/app.py
@@ -1,23 +1,15 @@
 #import os
 from flask import Flask
-from flask import jsonify
+from models import models
 
 #from variables import cargarvariables
 
 app = Flask(__name__)
-#app.config.from_pyfile('config/settings.py')
+app.config.from_pyfile('config/settings.py')
 
   
-from routes import user_routes
-
-#import waiter_routes
-
-#import product_routes
-
-
-#@app.route('/prueba', methods=['GET'])
-#def test():
-#    return jsonify({"message": "hello world"}), 200
+from routes import user_routes, waiter_routes
+app.register_blueprint(waiter_bp, url_prefix='/waiters')
 
 if __name__ == '__main__':
     #port = int(os.environ.get('PORT'))
